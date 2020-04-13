e9f89cb112462614e08de8419dbad547ef8cc778
Deer5000 <khidrbrinkley32@gmail.com>
2020-04-13 02:08:42 -0400

    Watched a few technical interviews and analyzed them.

---
+++ interview1.py
@@ -0,0 +1,60 @@
+""" INTERVIEW #1 ( Find pairs of values that sum up to a given value. )           )"""
+
+
+"""
+COMMUNICATION STEPS:
+
+1. Restate the problem
+2. Ask clarifying questions
+3. State your assumptions
+4. Think out loud
+    1. Brainstorm solutions
+    2. Explain your rationale
+    3. Discuss tradeoffs
+    4. Suggest improvements
+
+
+
+ANALYSIS:
+1. Restate problem
+
+- The interviewee did a great job of restating the problem.
+- He gave test inputs and got full clarity after restating the problem.
+- He wrote the interview question as a comment so that he could refer back to it if needed
+
+2. Ask clarifying problems
+
+- Asked " Can I assume the list will always have a valid answer?"
+- wrote the question as a comment as well as the interviewers response so that he could refer back to it if needed.
+- the interviewee could've asked a few more clarifying questions.
+
+3. State Assumptions
+
+    - Stated assumptions to interviewer to make sure he was on track and understood the problem.
+4. Think Out loud
+    1.Brainstorm solutions
+        - flowed smoothly between stating his assumptions and giving possible solutions.
+        - Walked through a few solutions and picked one that 
+        - gave more then one solutions and gave the time complexity of both.
+        - was able to choose the solution with the quickest time complexity.
+
+    2. Explain your rationale
+        - walked through each solution
+        - asked the interviewer if he wanted it optimized for time or space.
+        - was able to calculate the time complexity of both.
+        - chose the solution with the quickest time complexity
+
+    3. Discuss tradeoffs
+
+        - discussed tradeoofs between multiple solutions.
+        - asked a few more clarifying questions.
+        - chose the solution that was the absolute best for the problem.
+
+    4. Suggest improvements
+
+        - Walked the interviewer through his code.
+        - Asked if the interviewer had any questions and and answered them.
+        - suggested ways in which the code could be improved and asked if the interviewer if he wanted him to improve his work.
+
+
+"""
---
+++ interview2.py
@@ -0,0 +1,67 @@
+""" INTERVIEW #2 (Matching Pairs) """
+
+
+"""
+COMMUNICATION STEPS:
+
+1. Restate the problem
+2. Ask clarifying questions
+3. State your assumptions
+4. Think out loud
+    1. Brainstorm solutions
+    2. Explain your rationale
+    3. Discuss tradeoffs
+    4. Suggest improvements
+
+
+
+ANALYSIS:
+
+1. Restate The Problem:
+
+    - The interviewee restated the problem clearly.
+    - Rewrote the question as a comment so that he can refer back to it if needed.
+
+2. Ask Clarifying Questions:
+
+    - Asked "Can I assume the list will always have a valid answer?"
+
+
+3. State Your Assumptions:
+
+    - Theres no duplicated in the list.
+    - The list will not always have a valid answer.
+
+4. Think Out Loud:
+
+    1. Brainstorm:
+
+        - Walked through the naive answer with the interviewer
+        - Gave a more complex solution with a bettter time complexity
+        - Each solution that the interviewee brainstormed was well thought out.
+        - He was able to explain each solution he proposed clearly and thoroughly.
+   
+    2. Explain Your Rationale:
+
+        - Gave the time complexity of the solution he created
+        - walked through the solution.
+        - described the space-time complexity of each solution
+        - used a hashtable
+        - clarified what he was optimizing for
+
+    3. Discuss tradeoffs:
+        
+        - discussed tradeoffs between each solution
+        - asked if the interviewer wanted to optimize for time or space.
+
+    4. Suggest Improvements:
+        
+        - Asked if the interviewee wanted more than one valid answer.
+        - restated assumptions
+        - rewrote the solution and improved it.
+
+
+
+
+
+"""
